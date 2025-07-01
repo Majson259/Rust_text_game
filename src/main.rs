d9752ac62c17583c8f49e72ad091105e9a91415c
Marian Sehnal <sehnalmarian8@gmail.com>
2025-07-01 13:36:46 +0200

    Funkce křižovatka je funkční, zapisování do vektorů funguje jak má.

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ fn main() {
 
     let mut room = 0;
 
-    let mut smer = &mut String::from("nahoru");
+    let mut smer = String::from("nahoru");
 
     let mut mapa: Vec<Vec<i32>> = Vec::new();
     mapa.push(vec![0]);
 
-    let mut rng = rand::thread_rng();
+    let mut rng = rand::rng();
 
     let dej = ["Jsi v chodbě.", "Před tebou něco je, ", "Před tebou je nepřítel.", "Jsi na křižovatce."];
 
@@ -23,7 +23,9 @@ fn main() {
         let cislo = rng.random_range(0..dej.len() as i32);
         println!("{}", smer);
         
-        chodba(&dej, &mut x, &mut y, smer, &mut mapa, room);
+        
+
+        krizovatka(&dej, &mut x, &mut y, &mut smer, &mut mapa, room);
     }
 
 }
@@ -153,25 +155,151 @@ fn opacny_smer(smer: &mut String) -> String {
             return smer.to_string();
 }
 
-fn krizovatka(list: &[i32], x: i32, y: i32) {
+fn krizovatka(list: &[&str], mut x: &mut i32, mut y: &mut i32,smer: &mut String, mapa: &mut Vec<Vec<i32>>, mut room: i32) {
     println!("{}",list[3]);
     loop {
-        let odpoved = txt_vstup("Kterým směrem půjdeš?");
+        for i in &mut *mapa {
+            println!(" ");
+            for j in i {
+                print!("{}", j);
+            }
+        }
+        println!("x: {}", x);
+        println!("y: {}", y);
+        let mut odpoved = txt_vstup("Kterým směrem půjdeš?").trim().to_string();
         if odpoved == "rovně" {
             println!("Jdeš rovně.");
-            return;
+            if smer == "nahoru" {
+                if *y == 0 {
+                    let vektor = vec![0; (mapa[0].len()) as usize];
+                    mapa.insert(0, vektor);
+                    mapa[*y as usize][*x as usize] = 4;
+                    return;
+                } else if *y > 0 {
+                    *y -= 1;
+                    if mapa[*y as usize][*x as usize] == 0 {
+                        mapa[*y as usize][*x as usize] = 4;
+                        return;
+                    }
+                }
+            } else if smer == "dolů" {
+                if *y == mapa.len() as i32 - 1 {
+                    let vektor = vec![0; (*x + 1) as usize];
+                    *y += 1;
+                    mapa.insert(*y as usize, vektor);
+                    mapa[*y as usize][*x as usize] = 4;
+                    if mapa[*y as usize][*x as usize] == 0 {
+                        *y += 1;
+                        mapa[*y as usize][*x as usize] = 4;
+                        return;
+                    }
+                    return;
+                } else if *y != mapa.len() as i32 - 1 {
+                    if mapa[(*y + 1) as usize][*x as usize] != 0 {
+                        *y += 1;
+                        room = mapa[*y as usize][*x as usize];
+                        return;
+                    } else if mapa[(*y + 1) as usize][*x as usize] == 0 {
+                        *y += 1;
+                        mapa[*y as usize][*x as usize] = 4;
+                        return;
+                    }
+                }
+            } else if smer == "doleva" {
+                if *x == 0 {
+                    for vektor in &mut *mapa {
+                        vektor.insert(0, 0);
+                    }
+                    mapa[*y as usize][*x as usize] = 4;
+                    return;
+                } else if *x > 0 {
+                    if mapa[*y as usize][(*x - 1) as usize] != 0 {
+                        *x -= 1;
+                        room = mapa[*y as usize][*x as usize];
+                        return;
+                    } if mapa[*y as usize][(*x - 1) as usize] == 0 {
+                    *x -= 1;
+                    mapa[*y as usize][*x as usize] = 4;
+                    return;
+                    }
+                }
+            } else if smer == "doprava" {
+                if *x == mapa[0].len() as i32 - 1 {
+                    for vektor in &mut *mapa {
+                        vektor.push(0);
+                    }
+                    *x += 1;
+                    mapa[*y as usize][*x as usize] = 4;
+                    return;
+                } else if *x != mapa[0].len() as i32 - 1 {
+                    if mapa[*y as usize][(*x + 1) as usize] != 2 {
+                        *x += 1;
+                    } if mapa[*y as usize][*x as usize] == 0 {
+                        mapa[*y as usize][*x as usize] = 4;
+                    }
+                    room = mapa[*y as usize][*x as usize];
+                    return;
+                }
+            }
         } else if odpoved == "zpět" {
             println!("Otočil ses a jdeš zpátky");
+            opacny_smer(smer);
             return;
         } else if odpoved == "vpravo" {
             println!("Vykročil jsi vpravo");
+            zatacka(smer, &mut odpoved);
             return;
         } else if odpoved == "vlevo" {
             println!("Vykročil jsi vlevo");
+            zatacka(smer, &mut odpoved);
             return;
         }
          else {
             println!("Promiň, nerozuměl jsem")
+        }
+    }
+}
+
+fn zatacka(smer: &mut String, odpoved: &mut String) {
+    if *smer == "nahoru" {
+        if *odpoved == "vpravo" {
+            *smer = "doprava".to_string();          
+        } else if *odpoved == "vlevo" {   
+            *smer = "doleva".to_string();          
+        } else if *odpoved == "rovně" {
+            *smer = "nahoru".to_string();           
+        } else if *odpoved == "zpět" {
+            *smer = "dolů".to_string();          
+        }
+    } else if *smer == "doprava" {
+        if *odpoved == "vpravo" {
+            *smer = "dolů".to_string();           
+        } else if *odpoved == "vlevo" {   
+            *smer = "nahoru".to_string();           
+        } else if *odpoved == "rovně" {
+            *smer = "doprava".to_string();           
+        } else if *odpoved == "zpět" {
+            *smer = "doleva".to_string();          
+        }
+    } else if *smer == "dolů" {
+        if *odpoved == "vpravo" {
+            *smer = "doleva".to_string();           
+        } else if *odpoved == "vlevo" {   
+            *smer = "doprava".to_string();           
+        } else if *odpoved == "rovně" {
+            *smer = "dolů".to_string();           
+        } else if *odpoved == "zpět" {
+            *smer = "nahoru".to_string();           
+        }
+    } else if *smer == "doleva" {
+        if *odpoved == "vpravo" {
+            *smer = "nahoru".to_string();           
+        } else if *odpoved == "vlevo" {   
+            *smer = "dolů".to_string();           
+        } else if *odpoved == "rovně" {
+            *smer = "doleva".to_string();            
+        } else if *odpoved == "zpět" {
+            *smer = "doprava".to_string();
         }
     }
 }
